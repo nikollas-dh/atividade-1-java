91a6b20172ba8d15fa8b2cbcc4ad9b5265e0e1d7
unknown <dev@DSN1191348066.edu119.local>
2026-02-02 11:20:19 -0300

    crud finalizado

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,57 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+   public static ArrayList <String> usuarios = new ArrayList<>();
     public static void main(String[] args) {
-        while (true){
-        System.out.println("Digite 1 para adicionar um usuário:");
-        System.out.println("Digite 2 para listar um usuários:");
-        System.out.println("Digite 3 para deletar um usuário:");
-        System.out.println("Digite 4 para Sair um usuário:");
 
+        boolean a = true;
+        String nome;
+
+        while (a){
+        System.out.println("\nDigite 1 para adicionar um usuário:");
+        System.out.println("Digite 2 para listar um usuários:");
+        System.out.println("Digite 3 para editar um usuário:");
+        System.out.println("Digite 4 para deletar um usuário:");
+        System.out.println("Digite 5 para sair:\n");
         Scanner entrada = new Scanner(System.in);
-        int opcao = entrada.nextInt();
+        String opcao = entrada.nextLine();
 
         entrada = new Scanner(System.in);
-        ArrayList <String> usuarios = new ArrayList<>();
         switch (opcao) {
-            case 1:
+            case "1":
                 System.out.println("Digite o nome");
-                String nome = entrada.nextLine();
+                nome = entrada.nextLine();
                 usuarios.add(nome);
-            case 2:
+                System.out.println("Usuário adicionado com sucesso!");
+                break;
+            case "2":
+                System.out.println("Lista de usuários:");
                 System.out.println(usuarios);
+                break;
+            case "3":
+                if (usuarios.size()<=0){
+                    System.out.println("Não existe usuários no sistema");
+                }else {
+                System.out.println("Digite a posição do usuário que deseja editar:");
+                int posicao = Integer.parseInt(entrada.nextLine());
+                System.out.println("Digite o novo nome:");
+                String novoNome = entrada.nextLine();
+                usuarios.set(posicao, novoNome);
+                System.out.println("Usuário atualizado com sucesso!");
+                }
+                break;
+            case "4":
+                System.out.println("Digite o nome do usuário para deletar");
+                nome = entrada.nextLine();
+                usuarios.remove(nome);
+                System.out.println("Usuário removido com sucesso!");
+                break;
+            case "5":
+                entrada.close();
+                System.out.println("Saindo...");
+                a = false;
+            default:
+                System.out.println("Opção inválida!");
             }
         }
     }
